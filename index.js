eadfb15f53b116f6584efdda548c6523eb6b1e24
Zach Corpuz <zachcorpuz@yahoo.com>
2020-05-31 22:40:35 -0700

    Installed Inquirer for user input to start the generator app. Created all the inquirer questions for base of README

---
+++ index.js
@@ -0,0 +1,106 @@
+var inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?',
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need to enter a valid username';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your application?',
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need to enter a title for your application';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your application?',
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need a brief description of your application';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'tableOfContents',
+            message: 'What would you like to include in your README?',
+            choices: [
+                {name: 'Installation'},
+                {name: 'Usage'},
+                {name: 'License'},
+                {name: 'Contributing'},
+                {name: 'Tests'},
+                {name: 'Questions'},
+            ],
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need to choose at least 1 option';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'List specific steps for users to follow in order to use your application?',
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need to list at least one step for users';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Please list your license if your project is open source?',
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need to list your license';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'contributing',
+            message: 'Would you like to allow others to contribute to your project?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Explain how to run tests for your application',
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need to describe tests for your application';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'questons',
+            message: '?',
+            validate: function(answer) {
+                if (answer.length < 1) {
+                    return 'You need to list your license';
+                }
+                return true;
+            }
+        },
+    ])
